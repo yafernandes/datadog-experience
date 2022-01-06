969c8782cf9e9528dccef31e4810753766a7ce50
yafernandes <alexandrino.fernandes@gmail.com>
2022-01-06 10:08:00 -0500

    Move to ClusterConfiguration

--- labs/kubernetes/build.sh
+++ labs/kubernetes/build.sh
@@ -9,7 +9,6 @@ cd -
 cd ansible
 ansible-playbook -i inventory.txt os-setup.yaml --ssh-extra-args="-o IdentitiesOnly=yes"
 ansible-playbook -i inventory.txt kubernetes.yaml --ssh-extra-args="-o IdentitiesOnly=yes"
-ansible-playbook -i inventory.txt k8s-config.yaml --ssh-extra-args="-o IdentitiesOnly=yes"
 cd -
 
 kubectl config use-context kubernetes-admin@kubernetes
