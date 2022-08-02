f7a91cd561fd71e90417ceda19bb6d6dfcd40863
yafernandes <alexandrino.fernandes@gmail.com>
2022-08-02 14:01:26 -0400

    Added otel lab

---
+++ labs/otel/terraform/main.tf
@@ -0,0 +1,84 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.namespace}-otel"
+  location = var.region
+}
+
+resource "azurerm_public_ip" "main" {
+  name                = "otel-public-ip"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.namespace}-otel"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "otel-ip"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_sensitive_file" "key_file" {
+  filename        = "${var.namespace}-private_key.pem"
+  file_permission = "600"
+  content         = tls_private_key.ssh.private_key_pem
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                  = "${var.namespace}-otel"
+  resource_group_name   = azurerm_resource_group.main.name
+  location              = azurerm_resource_group.main.location
+  network_interface_ids = [azurerm_network_interface.main.id]
+  size                  = "Standard_B1ms"
+  admin_username        = var.username
+
+# az vm image list --output table
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  admin_ssh_key {
+    username   = var.username
+    public_key = tls_private_key.ssh.public_key_openssh
+  }
+
+  tags = {
+    creator = var.creator
+  }
+}
+
+resource "azurerm_dns_a_record" "main" {
+  name                = "otel.${var.namespace}"
+  zone_name           = var.domain
+  resource_group_name = var.domain-rg
+  ttl                 = 60
+  records             = [azurerm_public_ip.main.ip_address]
+}
+
+
+resource "local_file" "ansible_inventory" {
+  content  = templatefile("inventory.tmpl", { namespace = var.namespace, domain = var.domain })
+  filename = "../ansible/${var.namespace}-inventory.txt"
+}
---
+++ labs/otel/terraform/network.tf
@@ -0,0 +1,89 @@
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.namespace}-otel"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.namespace}-otel"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  security_rule {
+    name                       = "ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = chomp(data.http.myip.body)
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "otel-datadog-grpc"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5317"
+    source_address_prefix      = "*"
+    destination_address_prefix = azurerm_public_ip.main.ip_address
+  }
+
+  security_rule {
+    name                       = "otel-datadog-http"
+    priority                   = 301
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5318"
+    source_address_prefix      = "*"
+    destination_address_prefix = azurerm_public_ip.main.ip_address
+  }
+
+  security_rule {
+    name                       = "otel-collector-grpc"
+    priority                   = 302
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "4317"
+    source_address_prefix      = "*"
+    destination_address_prefix = azurerm_public_ip.main.ip_address
+  }
+
+  security_rule {
+    name                       = "otel-collector-http"
+    priority                   = 303
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "4318"
+    source_address_prefix      = "*"
+    destination_address_prefix = azurerm_public_ip.main.ip_address
+  }
+
+}
