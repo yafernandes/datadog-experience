2f3c7c010d6d06f97fd0168ecfb317207fd7f6b5
Alex Fernandes <alex.fernandes@datadoghq.com>
2022-10-27 12:48:43 -0400

    Removed any manual instrumentation

--- labs/otel/app/src/main/java/com/datadoghq/ese/servlet/PublicIPServlet.java
+++ labs/otel/app/src/main/java/com/datadoghq/ese/servlet/PublicIPServlet.java
@@ -13,8 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
-import io.opentelemetry.api.trace.Span;
-
 @WebServlet("/myip")
 public class PublicIPServlet extends HttpServlet {
 
@@ -22,27 +20,13 @@ public class PublicIPServlet extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    setOTelDatadogLogContext();
+
     resp.setContentType("text/html");
     logger.info("Logging with slf4j");
     String ip = Request.Get("http://ipv4.icanhazip.com").execute().returnContent().asString();
     logger.info("My ip is " + ip);
     resp.getWriter().println(ip);
     MDC.clear();
-  }
-
-  void setOTelDatadogLogContext() {
-    if (Span.current().isRecording()) {
-      String traceIdValue = Span.current().getSpanContext().getTraceId();
-      String traceIdHexString = traceIdValue.substring(traceIdValue.length() - 16);
-      long datadogTraceId = Long.parseUnsignedLong(traceIdHexString, 16);
-      MDC.put("dd.trace_id", Long.toUnsignedString(datadogTraceId));
-
-      String spanIdValue = Span.current().getSpanContext().getSpanId();
-      String spanIdHexString = spanIdValue.substring(spanIdValue.length() - 16);
-      long datadogSpanId = Long.parseUnsignedLong(spanIdHexString, 16);
-      MDC.put("dd.span_id", Long.toUnsignedString(datadogSpanId));
-    }
   }
 
 }
