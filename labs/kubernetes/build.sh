821a192e66e9a35b4f08268f319757ece0493e18
Alex Fernandes <alex.fernandes@datadoghq.com>
2023-06-12 07:37:34 -0400

    New scripts

--- labs/kubernetes/build.sh
+++ labs/kubernetes/build.sh
@@ -2,11 +2,21 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-source $1.env
+if [ ! -f $1.env ]
+then
+  echo -e "\033[0;31mEnv file does not exist\033[0m\007"
+  ls  -1 *.env | xargs -n 1 basename
+  exit 1
+else
+  source $1.env
+  DDEXP_NAMESPACE=$1
+fi
 
 cd terraform
+
+terraform workspace select -or-create=true $DDEXP_NAMESPACE
+
 terraform apply --auto-approve \
-  -state="$DDEXP_NAMESPACE.tfstate" \
   -var="region=$DDEXP_REGION" \
   -var="namespace=$DDEXP_NAMESPACE" \
   -var="creator=$DDEXP_CREATOR" \
@@ -14,8 +24,6 @@ terraform apply --auto-approve \
 cd -
 
 cd ansible
-
-KUBE_CLUSTERNAME=$DDEXP_NAMESPACE-kubeadm
 
 ansible-playbook -i $DDEXP_NAMESPACE-inventory.txt os-setup.yaml \
   --ssh-extra-args="-o IdentitiesOnly=yes" \
@@ -27,15 +35,17 @@ ansible-playbook -i $DDEXP_NAMESPACE-inventory.txt cri-containerd.yaml \
 
 ansible-playbook -i $DDEXP_NAMESPACE-inventory.txt kubernetes.yaml \
   --ssh-extra-args="-o IdentitiesOnly=yes" \
-  -e kube_clustername=$DDEXP_NAMESPACE-kubeadm \
+  -e kube_clustername=$DDEXP_NAMESPACE \
   -e kube_version=$DDEXP_KUBE_VERSION \
   -e ansible_ssh_private_key_file=../terraform/$DDEXP_NAMESPACE-private_key.pem
 cd -
 
-chmod 0600 ~/.kube/$KUBE_CLUSTERNAME.config
+chmod 0600 ~/.kube/$DDEXP_NAMESPACE.config
 
 zip -j9 $1.zip \
-  ~/.kube/$KUBE_CLUSTERNAME.config \
+  ~/.kube/$DDEXP_NAMESPACE.config \
   terraform/$DDEXP_NAMESPACE-private_key.pem \
   ansible/$DDEXP_NAMESPACE-inventory.txt
+
+rm ansible/$DDEXP_NAMESPACE-inventory.txt
 
--- labs/kubernetes/destroy.sh
+++ labs/kubernetes/destroy.sh
@@ -2,13 +2,40 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-source $1.env
+if [ ! -f $1.env ]; then
+  echo -e "\033[0;31mEnv file does not exist\033[0m\007"
+  ls  -1 *.env | xargs -n 1 basename
+  exit 1
+else
+  source $1.env
+  DDEXP_NAMESPACE=$1
+fi
 
 cd terraform
+
+terraform workspace select $DDEXP_NAMESPACE
+
 terraform destroy --auto-approve \
-  -state="$DDEXP_NAMESPACE.tfstate" \
   -var="region=$DDEXP_REGION" \
   -var="namespace=$DDEXP_NAMESPACE" \
   -var="creator=$DDEXP_CREATOR" \
   -var="workers_count=$DDEXP_WORKERS_COUNT"
+
 cd -
+
+resources=$(cat terraform/terraform.tfstate.d/$DDEXP_NAMESPACE/terraform.tfstate | jq '.resources | length')
+
+if (( $resources == 0 )); then
+    set +eo pipefail
+
+    cd terraform
+    terraform workspace select default
+    terraform workspace delete $DDEXP_NAMESPACE
+    cd -
+
+    rm terraform/$DDEXP_NAMESPACE-private_key.pem
+    rm ansible/$DDEXP_NAMESPACE-inventory.txt
+    rm $DDEXP_NAMESPACE.zip
+    rm ~/.kube/$DDEXP_NAMESPACE.config
+fi
+
